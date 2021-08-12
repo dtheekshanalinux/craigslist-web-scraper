8cf91042277a351474068af4eed6c9af0821d30f
dtheekshanalinux <dinindu.linux@gmail.com>
2021-08-12 16:24:48 +0530

    add hosts

--- craigslist/settings.py
+++ craigslist/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ["dtcraigslist.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
